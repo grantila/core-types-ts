13e4664d7567f5a6f92fc504ede5b8755a3f8efe
Gustaf Räntilä <g.rantila@gmail.com>
2021-04-17 10:00:05 +0200

    chore(jest): moved jest config to package json, and use .test.ts instead of .spec.ts

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	preset: 'ts-jest',
-	testEnvironment: 'node',
-	testMatch: [ '<rootDir>/lib/**/*.spec.ts' ],
-	collectCoverageFrom: [ '<rootDir>/lib/**' ],
-	coveragePathIgnorePatterns: [ '/node_modules/', '/__snapshots__/' ],
-	coverageReporters: [ 'lcov', 'text', 'html' ],
-};
